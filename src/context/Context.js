80bcf028a0c60147e13ebfb69746b308df386e6b
vishal <vishaliscoding27@gmail.com>
2023-06-27 16:24:28 +0530

    Cookie to save loggedi ďDetails

--- src/context/Context.js
+++ src/context/Context.js
@@ -1,9 +1,14 @@
+import Cookies from 'js-cookie';
 import React, {useState, createContext} from 'react'
 
 export const UserContext = createContext();function AuthContext({children}) {
-const [auth, setAuth] = useState({id: "", username: "", email: "", token: ""})
-
-
+const str = JSON.parse(Cookies.get("token") || "{}");
+const [auth, setAuth] = useState({
+    id: str.id || "",
+    username: str.username || "",
+    email: str.email || "",
+    token: str.token || ""
+})
 return (<UserContext.Provider value={
     {auth, setAuth}
 }> {children} </UserContext.Provider>)}export default AuthContext
